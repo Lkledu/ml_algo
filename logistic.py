ea6256f587cc3c0d272a89ef637772455e8421d0
Eduardo Souza <lkledu@gmail.com>
2019-10-15 09:03:34 -0300

    logistic regression

---
+++ logistic.py
@@ -0,0 +1,26 @@
+def somatoria(listToCount):
+    count = 0
+    for x in listToCount:
+        count += x
+    return count
+
+def slope():
+    b = 0
+    return 0
+
+def interception():
+    a = 0
+    return 0
+
+def yhat():
+    y = 0
+    return 0
+
+def gradient():
+    r = 0
+    return 0
+
+list = {1,2}
+b = somatoria(list)
+print("ASDOASJDOAISJD")
+print(b)
