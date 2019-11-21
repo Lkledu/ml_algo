b1c771a2a75c90fc0a475a86d0267e58a5d4f1f8
Eduardo Souza <lkledu@gmail.com>
2019-11-21 14:40:40 -0300

    trying pass object as argument

--- decisionTree.py
+++ decisionTree.py
@@ -1,45 +1,45 @@
-class Node:
-    def __init__(self, data, isQuestion = False):
+class Node(object):
+    data  = ""
+    isQuestion = False
+    left = None
+    right = None
 
-        self.left = None
-        self.right = None
+    def __init__(self, data = "", isQuestion = False, left = None, right = None):
+        self.left = left
+        self.right = right
         self.data = data
         self.isQuestion = isQuestion
 
-
-    def PrintTree(self):
-        print(self.data)
-
 root = Node("tem asa?", True)
-print("#######")
 
-def runTree(currentNode):
-    if (currentNode.right is not None):
-        print('foi')
-    if (currentNode is not None):
-        if(currentNode.isQuestion == True):
+def runTree(curNode):
+    if (curNode is not None):
+        if(curNode.isQuestion == True):
             print("\n\t")
-            print(currentNode.data)
+            print(curNode.data)
             answer = input("\nEnter your input(1-yes, 0-no):\n")
             if(answer == '1'):
-                runTree(currentNode.right)
+                runTree(curNode.right)
             elif(answer == '0'):
-                runTree(currentNode.left)
+                runTree(curNode.left)
     
         else:
                 print("\n\t")
-                print(currentNode.data)
+                print(curNode.data)
         
     else:
         print("\n###########################\nERROR: node isn't a question and not has a label yet.\n###########################\n")
         answer = input("Want to put a question(1) or a label(0)?\n")
         if(answer == '1'):
             question = input("write your question\n")
-            currentNode = Node(question, True)
-            print(currentNode)
+            curNode = Node(question, True)
+            print(curNode)
+            print(root)
         elif(answer == '0'):
             label = input("write your label\n")
-            currentNode = Node(label, False)
+            curNode = Node(label, False)
+            print(curNode)
+            print(root)
         print("\n\nrunning the decisionTree again\n###########################\n")
         runTree(root)
         
