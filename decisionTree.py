89c5cb6908dfdec3f14342639109da34c54af6c2
Milena Petrin <0040961913026@americana.fatec.br>
2019-11-22 21:44:31 -0300

    removing debug prints

--- decisionTree.py
+++ decisionTree.py
@@ -13,8 +13,8 @@ class Node(object):
 root = Node("has wings?", True)
 
 def runTree(curNode):
-    isinstance(curNode, object)
-    isinstance(curNode.right, object)
+    #isinstance(curNode, object)
+    #isinstance(curNode.right, object)
 
     if (curNode.data != ""):
         print(curNode)
@@ -37,7 +37,7 @@ def runTree(curNode):
         
     else:
         print(curNode)
-        print("\n###########################\nERROR: node isn't a question and not has a label yet.\n###########################\n")
+        print("\n\n\tERROR: node isn't a question and not has a label yet.\n\n")
         answer = input("Want to put a question(1) or a label(0)?\n")
         if(answer == '1'):
             question = input("write your question\n")
@@ -55,7 +55,7 @@ def runTree(curNode):
             
             print(curNode)
             print(root)
-        print("\n\nrunning the decisionTree again\n###########################\n")
+        print("\n\n\trunning the decisionTree again\n\n")
         runTree(root)
         
 runTree(root)
