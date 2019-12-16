11668714f7115d2626637c1101f5747a0d892591
Eduardo Teixeira de Souza <lkledu@gmail.com>
2019-12-16 06:39:00 -0200

    calculating gain info

--- decisionTree2.py
+++ decisionTree2.py
@@ -14,33 +14,83 @@ dt.append(dataset('alto', 'perto', 'desinteressante', 'não'))
 dt.append(dataset('baixo', 'longe', 'desinteressante', 'sim'))
 dt.append(dataset('alto', 'perto', 'interessante', 'sim'))
 dt.append(dataset('baixo', 'longe', 'desinteressante', 'não'))
+#dt.append(dataset('alto', 'perto', 'interessante', 'sim'))
 
 def organize(attrib:str, objArray: []):
     listOrganized = []
     if hasattr(objArray[0], attrib):
         for x in objArray:
             listOrganized.append(getattr(x, attrib))
-    
     return listOrganized
 
-def peso(label: str, column: list):
+
+
+def getLabels(column: [], attr:str):
+    labels = []
+    count = 0
+    if hasattr(column[0], attr):
+        labels.append(getattr(column[0], attr))
+        for x in column:
+            for y in labels:
+                if  getattr(x, attr) == y:
+                    count +=1
+            if count == 0:
+                labels.append(getattr(x, attr))
+            count = 0
+            
+    return labels
+
+
+
+def peso(label: str, labelTarget: str, column: list, attr:str):
     countLabel = 0
     for x in column:
-        if(x == label):
+        if(x.decisao == labelTarget) and getattr(x, attr) == label:
             countLabel+=1
-
     return countLabel/len(column)
 
-def entropia(peso:int):
-    entrop = -(peso * math.log2(peso))
 
+
+def entropia(column: [], dt: [], attr:str):
+    entrop = 0
+    labels = getLabels(dt, attr)
+    labelTarget = getLabels(dt, 'decisao')
+    for x in labels:
+        for y in labelTarget:
+            if(peso(str(x), str(y), dt, attr) != 0):
+                entrop += -(peso(str(x), str(y) ,dt, attr) * math.log2(peso(str(x), str(y), dt, attr)))
     return entrop
 
-def ganhoInfo(columPai: list, columnFilho:list):
-    return entropia(columPai) - peso("label",columnFilho) * entropia(columnFilho)
+
+
+def ganhoInfo(entroT: float, entroC: float, dt: [], attr: str):
+    gi = 0
+    label = getLabels(dt, attr)
+    labelTarget = getLabels(dt, 'decisao')
+    var = 0
+    for x in label:
+        for y in labelTarget:
+            var += peso(x, y, dt, attr) * entroC
+    gi = entroT - var
+    return gi
 
 target = organize('decisao', dt)
+salarioList = organize('salario', dt)
+localizacaoList = organize('localizacao', dt)
+funcaoList = organize('funcao', dt)
 
-entropiaTarget = entropia(peso('sim', target)) + entropia(peso('não', target))
+
+entropiaTarget = entropia(target, dt, 'decisao')
+entropiaSalario = entropia(salarioList, dt, 'salario')
+entropiaLocal = entropia(localizacaoList, dt, 'localizacao')
+entropiaFuncao = entropia(funcaoList, dt, 'funcao')
+
+giSalario = ganhoInfo(entropiaTarget, entropiaSalario, dt, 'salario')
+giLocal = ganhoInfo(entropiaTarget, entropiaLocal, dt, 'localizacao')
+giFuncao = ganhoInfo(entropiaTarget, entropiaFuncao, dt, 'funcao')
+
 
 print(entropiaTarget)
+print(giSalario)
+print(giLocal)
+print(giFuncao)
